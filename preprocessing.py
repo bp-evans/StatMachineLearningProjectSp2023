15a0294da88ed52fd07a55220c52800d1590ca0a
cgretzem <cgretzem@asu.edu>
2023-03-22 20:40:02 -0700

    Processing removes duplicates

--- preprocessing.py
+++ preprocessing.py
@@ -5,10 +5,14 @@ def process(filename):
     
     df = pd.read_csv(filename, header=None)
     tw_list = pd.DataFrame(df[1])
+    new_list = []
+    for tweet in tw_list:
+        if tweet not in new_list:
+            new_list.append(tweet)
     with open(f'{filename}_cleaned.csv', 'w') as f:
         for tweet in tw_list[1]:
             new_tweet = p.clean(tweet).replace(',','')
             f.write(f'{new_tweet},')
 
 if __name__ == '__main__':
-    process('smile_annotations.csv')
+    process('smileannotationsfinal.csv')
